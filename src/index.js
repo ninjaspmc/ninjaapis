c95b4748c4772028a7aba4b203abfd7a9d300be8
Ninja Spmc <ninjaspmc@gmail.com>
2024-03-23 16:02:17 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.get("/teste", (req, res) => {
   res.send({ teste: false });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
